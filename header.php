39fb0c3ed83c5036d07d35b15c03b5cc5f639908
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-03-30 11:44:12 +0200

    Added header

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="img/logo.png" alt="">
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="usp.php">Unique selling points</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    </header>
+</body>
+</html>
