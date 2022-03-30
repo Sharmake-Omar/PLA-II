802f7995aba3386f84b8ef237b8eb1d20c5d6b40
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-03-30 12:01:20 +0200

    Added header

--- header.php
+++ header.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SuperCoaster</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     <header>
         <img src="img/logo.png" alt="">
         <nav>
-            <a href="home.php">Home</a>
+            <a href="index.php">Home</a>
             <a href="usp.php">Unique selling points</a>
+            <a href="wachttijd.php">Wachttijd</a>
             <a href="contact.php">Contact</a>
         </nav>
     </header>
