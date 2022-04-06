c2aaab862ef89578842d97759569b9f8a1e520c7
DikkeBertus <86045812+DikkeBertus@users.noreply.github.com>
2022-04-06 12:57:37 +0200

    started usp page
    added header to queue page

---
+++ usp.php
@@ -0,0 +1,10 @@
+<?php
+require_once("header.php")
+?>
+
+<div class="usp">
+    <h1>RollerCoaster NV, constante innovatie</h1>
+    <p>RollerCoaster NV verandert op elk niveau de manieren hoe achtbanen gemaakt worden.</p>
+    <p>Hun nieuwste achtbaan, de SuperCoaster is een achtbaan zoals je nooit eerder hebt gezien.</p>
+    <p>Met een nog betere constructie, mogelijk gemaakt door metaal messing die door nieuwe methodes sterker is dan ooit tevoren.</p>
+</div>
